0c754ffb2cc431c514d9042efef9a884ec385bb8
Akassh09 <duttaakash.8348@gmail.com>
2020-10-19 22:14:54 +0530

    first commit

--- todo/task/forms.py
+++ todo/task/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.forms import ModelForm
 from .models import *
 
-# Edit: line 8  has been included later to add placeholder for the input Field
+# Edit: line 8  has been included later to add placeholder for the input Field..
 
 class TaskForm(forms.ModelForm): 
     title = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Add new task...'}))
